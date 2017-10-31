ef1c4de1c0f36814fd0d680a64ea8e7318d3369f
Marta <martadabrowka@gmail.com>
2017-10-31 17:27:26 +0000

    created Director class inheriting from Manager

---
+++ employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Director.java
@@ -0,0 +1,11 @@
+package example.codeclan.com.employeestartpoint;
+
+/**
+ * Created by marta on 31/10/2017.
+ */
+
+public class Director extends Manager {
+    public Director(int id, String name, String socialSecurityNumber, double salary, String deptName) {
+        super(id, name, socialSecurityNumber, salary, deptName);
+    }
+}
